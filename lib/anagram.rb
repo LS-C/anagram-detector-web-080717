5b736abad98870f5b4250c58be4213950b4149d1
LS-C <suzy.cho111@gmail.com>
2017-08-13 12:59:14 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        anagram=[]
+        a= array.select { |word| word.length == @word.length }
+        a.each do |ana|
+        if ana.chars.sort == @word.chars.sort
+            anagram << ana
+        end
+    end
+    anagram
+end
+
+
+
+end
+
